8b86551d5b4a5c75d2c90a274b692ed9376ee07d
DanielBlomdahl <h.daniel.blomdahl@gmail.com>
2025-02-08 16:27:08 +0100

    Changed car to have abstract constructor

--- lab1/Saab95.java
+++ lab1/Saab95.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class Saab95 extends car{
+public class Saab95 extends Car {
 
     public boolean turboOn;
     public Saab95(){
@@ -8,10 +8,6 @@ public class Saab95 extends car{
         setColor(Color.red);
         setEnginePower(125);
 	    turboOn = false;
-        setModelName("Saab95");
-        setCurrentDirection(Direction.UP);
-        setPosition(new double[] {0,0});
-        stopEngine();
     }
 
     public void setTurboOn(){
@@ -22,25 +18,11 @@ public class Saab95 extends car{
 	    turboOn = false;
     }
     
-    private double speedFactor(){
+    public double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return super.getEnginePower() * 0.01 * turbo;
     }
 
-    public void incrementSpeed(double amount){
-        setCurrentSpeed(getCurrentSpeed() + speedFactor() * amount);
-    }
 
-    public void decrementSpeed(double amount){
-        setCurrentSpeed(getCurrentSpeed() - speedFactor() * amount);
-    }
-
-    public void gas(double amount){
-        incrementSpeed(Math.max(0,Math.min(amount,1)));
-    }
-
-    public void brake(double amount){
-        decrementSpeed(Math.max(0,Math.min(amount,1)));
-    }
 }
